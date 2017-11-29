d665270e2b4480290119a749cdad6c5789f2a688
vpovio <vpovio@gmail.com>
2017-11-29 14:52:39 -0800

    Added ability to save login information

--- app/src/main/java/group2/tcss450/uw/edu/cache_ing_app/home/LoginFragment.java
+++ app/src/main/java/group2/tcss450/uw/edu/cache_ing_app/home/LoginFragment.java
@@ -2,13 +2,16 @@ package group2.tcss450.uw.edu.cache_ing_app.home;
 
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -19,6 +22,10 @@ import group2.tcss450.uw.edu.cache_ing_app.R;
  * A simple {@link Fragment} subclass.
  */
 public class LoginFragment extends Fragment implements View.OnClickListener {
+
+    private CheckBox cb = (CheckBox) getView().findViewById(R.id.save_login_cb);
+    private EditText userNameLogin = (EditText) getView().findViewById(R.id.username_editText);
+    private EditText passwordLogin = (EditText) getView().findViewById(R.id.password_editText);
 
     private OnFragmentInteractionListener mListener;
 
@@ -42,6 +49,7 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
         b.setOnClickListener(this);
         TextView fp = (TextView) v.findViewById(R.id.forgot_pass);
         fp.setOnClickListener(this);
+        loadPreferences();
         return v;
     }
 
@@ -56,12 +64,15 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
             Log.d("DO", "onClick: ");
             switch(v.getId()) {
                 case R.id.sign_in:
+                    savePreferences("CHECKBOX", cb.isChecked());
                     Log.d("DO SOMETHING", "PLEASE");
-                    EditText userNameLogin = (EditText) getView().findViewById(R.id.username_editText);
-                    EditText passwordLogin = (EditText) getView().findViewById(R.id.password_editText);
 
                     String userName = userNameLogin.getText().toString();
                     String password = passwordLogin.getText().toString();
+                    if (cb.isChecked()) {
+                        savePreferences("USERNAME", userName);
+                        savePreferences("PASSWORD", password);
+                    }
                     if(userName.isEmpty()) {
                         userNameLogin.setError("Enter your username/email");
                     } if (password.isEmpty()) {
@@ -111,6 +122,31 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
     public interface OnFragmentInteractionListener {
 
         void loginFragmentInteraction(String c, String c2);
+    }
+
+    private void loadPreferences() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.getContext());
+        boolean boxValue = sharedPreferences.getBoolean("CHECKBOX", false);
+        String userName = sharedPreferences.getString("USERNAME", "");
+        String password = sharedPreferences.getString("PASSWORD", "");
+
+        cb.setChecked(boxValue);
+        userNameLogin.setText(userName);
+        passwordLogin.setText(password);
+    }
+
+    private void savePreferences(String key, boolean value) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.getContext());
+        SharedPreferences.Editor perry = sharedPreferences.edit();
+        perry.putBoolean(key, value);
+        perry.commit();
+    }
+
+    private void savePreferences(String key, String value) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.getContext());
+        SharedPreferences.Editor perry = sharedPreferences.edit();
+        perry.putString(key, value);
+        perry.commit();
     }
 }
 
